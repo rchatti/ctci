09ac41ca7bb1013a1b69e3e4b151243b80c82b55
rchatti <ravi@ravic-501.spenceinfo.spins.com>
2020-01-10 14:33:57 -0600

    Working Solution

---
+++ arrays_one_way.py
@@ -0,0 +1,44 @@
+"""
+Input: Str1 and Str2 
+
+Idea: if Lengths of Str1 and Str2 are off by more than 1, then return false 
+        Add Str1 strings and Count to Dict
+        Remove Str2 letters from Dict. 
+
+        If Len of Dict > 1: then False, else True 
+
+"""
+
+def one_way(str1, str2):
+    
+    # if Lengths of Str1 and Str2 are off by more than 1, then return false 
+    if abs(len(str1) - len(str2)) > 1:
+        return False
+
+    str_dict = {}
+
+    # Add Str1 strings and Count to Dict
+    for letter in list(str1):
+        if letter in str_dict:
+            str_dict[letter] += 1
+        else:
+            str_dict[letter] = 1
+
+    # Remove Str2 letters from Dict. 
+    for letter in list(str2):
+        if letter in str_dict.keys():
+            str_dict[letter] -= 1
+        else:
+            str_dict[letter] = -1
+
+    # If Len of Dict > 1: then False, else True 
+    for (k,v) in str_dict.items():
+        if v > 1 or v < -1:
+            return False
+        else:
+            return True
+
+
+print(one_way('ravi','ivar'))
+print(one_way('ravi','ivari'))
+print(one_way('ravi','ivariv'))
